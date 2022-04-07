a1e63b14938c7c60b65118d667ebdca29181d94f
Manaswini Sahoo <84119894+2002Manaswini@users.noreply.github.com>
2022-04-07 11:42:46 +0530

    Update LAB1-q2.R

--- LAB1-q2.R
+++ LAB1-q2.R
@@ -1,3 +1,6 @@
+#Write an R-script to convert given paisa into its equivalent rupee and paisa as per
+#the following format. 550 paisa = 5 Rupee and 50 paisa.
+
 {
 tp <- as.integer(readline(prompt = "Enter paisa :"))
 

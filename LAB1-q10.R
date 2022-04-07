3ad82e357c7a45600f99dc4b470726ef635b54f8
Manaswini Sahoo <84119894+2002Manaswini@users.noreply.github.com>
2022-04-07 15:55:53 +0530

    Update LAB1-q10.R

--- LAB1-q10.R
+++ LAB1-q10.R
@@ -1,3 +1,5 @@
+#10)Write an R-script to enter two numbers and find out the biggest one.
+
 {
 x <- as.integer(readline(prompt = "Enter first number :"))
 y <- as.integer(readline(prompt = "Enter second number :"))

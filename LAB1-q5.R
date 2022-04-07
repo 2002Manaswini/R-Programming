ade2a487a1fa1039175efeeadf8bf4c7766ba875
Manaswini Sahoo <84119894+2002Manaswini@users.noreply.github.com>
2022-04-07 15:51:20 +0530

    Update LAB1-q5.R

--- LAB1-q5.R
+++ LAB1-q5.R
@@ -1,3 +1,6 @@
+#A cashier has currency notes of denominations 10, 50 and 100. If the amount to be withdrawn is input through the keyboard in hundreds, 
+#write an R-script to find the total number of currency notes of each denomination the cashier will have to give to the withdrawer.
+
 {
 w <- as.integer(readline(prompt = "Enter withdrawl amount :"))
 

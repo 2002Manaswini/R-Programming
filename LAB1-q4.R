2f88afb32a9ea6ef2386bf116da6a48f93c0cb64
Manaswini Sahoo <84119894+2002Manaswini@users.noreply.github.com>
2022-04-07 15:50:36 +0530

    Update LAB1-q4.R

--- LAB1-q4.R
+++ LAB1-q4.R
@@ -1,3 +1,6 @@
+#Write an R-script to convert a quantity in meter entered through keyboard into its equivalent kilometer & meter as per the following format. 
+#Example - 2430 meter = 2 Km and 430 meter.
+
 {
 m <- as.integer(readline(prompt = "Enter meter :"))
 
